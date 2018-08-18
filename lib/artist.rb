f4827f7e50ae9e64ead34f37cee55898126ce844
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 02:51:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
-  include Parambale
+  include Paramable
 
   attr_accessor :name
   attr_reader :songs
