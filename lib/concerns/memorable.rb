e64eab8f306ed184b64a92fa2a4e97ae277b359d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 02:39:08 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,11 +2,11 @@ module Memorable
 
     module ClassMethods
       def reset_all
-        all.clear
+        self.all.clear
       end
 
       def count
-        all.count
+        self.all.count
       end
     end
 
