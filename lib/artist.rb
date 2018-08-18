8a440326c1852dd64be26ae9530b9d5b7e57f343
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 02:31:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,10 +15,10 @@ class Artist
   #   @@artists.detect{|a| a.name == name}
   # end
 
-  # def initialize
-  #   @@artists << self
-  #   @songs = []
-  # end
+  def initialize
+    @@artists << self
+    @songs = []
+  end
 
   def self.all
     @@artists
