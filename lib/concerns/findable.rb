1fc92e6d953c1d633710403e98ecdfd7f208a067
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 02:19:31 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,7 @@
 module Findable
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+  def find_by_name(name)
+    all.detect{|a| a.name == name}
   end
 
 end
